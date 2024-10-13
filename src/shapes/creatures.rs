a3f148532e354c2fd4cbf97e3b6b97570f808061
Rik de Goede <rik.dgd@gmail.com>
2024-10-13 18:48:10 +0200

    Embed sprites at compile time.

--- src/shapes/creatures.rs
+++ src/shapes/creatures.rs
@@ -1,11 +1,8 @@
 use crate::shapes::pixel_image::{Pixel, PixelImage};
 use ratatui::prelude::Color;
 use serde::{Deserialize, Serialize};
-use image::ImageReader;
+use image::load_from_memory;
 use std::error::Error;
-
-
-const DUCK_SPRITE_PATH: &str = "../assets/duck.png";
 
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,35 +13,41 @@ pub enum CreatureShapes {
 impl PixelImage for CreatureShapes {
     fn pixels(&self) -> Vec<Pixel> {
         match self {
-            CreatureShapes::Duck => load_sprite(DUCK_SPRITE_PATH, Color::Cyan).unwrap(),
+            CreatureShapes::Duck => {
+                let duck_sprite = include_bytes!("../../assets/duck.png");
+                load_sprite(duck_sprite, Color::Cyan).unwrap()
+            },
         }
     }
 }
 
-fn load_sprite(path: &str, color: Color) -> std::io::Result<Vec<Pixel>> {
+fn load_sprite(image_bytes: &[u8], color: Color) -> std::io::Result<Vec<Pixel>> {
     let mut pixels = Vec::new();
-    let img_data = load_image_data(DUCK_SPRITE_PATH).map_err(|_| {
-        std::io::Error::new(std::io::ErrorKind::NotFound, "Sprite image not found")
+    let black_pixel_cords = get_black_pixel_coordinates(image_bytes).map_err(|_| {
+        std::io::Error::new(
+            std::io::ErrorKind::NotFound, 
+            "Sprite image not found"
+        )
     })?;
 
-    for cords in img_data {
+    for cords in black_pixel_cords {
         pixels.push(Pixel {
             x: cords.0,
             y: cords.1,
             color,
         });
     }
-    
+
     Ok(pixels)
 }
 
 
 /// Returns the coordinates of each black pixel in a vector.
-fn load_image_data(path: &str) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
-    let image = ImageReader::open(path)?.decode()?.to_luma8();
+fn get_black_pixel_coordinates(image_bytes: &[u8]) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
+    let image = load_from_memory(image_bytes).unwrap().to_luma8();
     let dimensions = image.dimensions();
     let mut buffer: Vec<(u32, u32)> = Vec::new();
-    
+
     for (pixel_index, pixel) in image.pixels().enumerate() {
         if pixel.0[0] == 0 {
             let cords = get_pixel_coordinates(pixel_index, dimensions);
@@ -59,7 +62,7 @@ fn get_pixel_coordinates(pixel_index: usize, img_dimensions: (u32, u32)) -> (u32
     let pixel_index = pixel_index as u32;
     let y_cord = pixel_index / img_dimensions.0;
     let x_cord = pixel_index % img_dimensions.0;
-    
+
     (x_cord, y_cord)
 }
 
@@ -72,7 +75,7 @@ mod test {
         let dimensions_10x10: (u32, u32) = (10, 10);
         let dimensions_5x5: (u32, u32) = (5, 5);
         let dimensions_width_1: (u32, u32) = (1, 30);
-        
+
         let result_a = get_pixel_coordinates(24, dimensions_10x10);
         let result_b = get_pixel_coordinates(6, dimensions_5x5);
         let result_c = get_pixel_coordinates(1, dimensions_5x5);
