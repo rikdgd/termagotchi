83d679ac1ab556943827c935e752d9b48cd7e564
Rik de Goede <rik.dgd@gmail.com>
2024-11-01 23:17:53 +0100

    Add growth stages and make them drawable.

---
+++ src/shapes/growth_stages.rs
@@ -0,0 +1,44 @@
+use ratatui::widgets::canvas::{Painter, Shape};
+use serde::{Deserialize, Serialize};
+use crate::shapes::PixelImage;
+use crate::utils::{ColorWrapper, Pixel, sprite_management::load_sprite};
+
+
+macro_rules! load_embedded_sprite {
+    ($sprite_path:expr, $color:expr) => {
+        {
+            let sprite = include_bytes!($sprite_path);
+            load_sprite(sprite, $color.get_ratatui_color()).unwrap()
+        }
+    };
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum GrowthStageShapes {
+    Egg(ColorWrapper),
+    Baby(ColorWrapper),
+    Kid(ColorWrapper),
+}
+
+impl PixelImage for GrowthStageShapes {
+    fn pixels(&self) -> Vec<Pixel> {
+        match self {
+            GrowthStageShapes::Egg(color) => load_embedded_sprite!("../../assets/egg.png", color),
+            GrowthStageShapes::Baby(color) => load_embedded_sprite!("../../assets/baby.png", color),
+            GrowthStageShapes::Kid(color) => load_embedded_sprite!("../../assets/egg.png", color), // TODO: create kid shape and use that
+        }
+    }
+}
+
+impl Shape for GrowthStageShapes {
+    fn draw(&self, painter: &mut Painter) {
+        for pixel in self.pixels() {
+            painter.paint(
+                pixel.x as usize, 
+                pixel.y as usize, 
+                pixel.color
+            );
+        }
+    }
+}
--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -1,4 +1,5 @@
 pub mod creatures;
 mod pixel_image;
+mod growth_stages;
 
 pub use pixel_image::PixelImage;
