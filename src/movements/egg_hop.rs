ba71692170b2062c6dcbf09ad01d71b25bb812a9
Rik de Goede <rik.dgd@gmail.com>
2024-11-10 00:18:28 +0100

    EggHopMovement now hops each 0.5 seconds, instead of just every render.

--- src/movements/egg_hop.rs
+++ src/movements/egg_hop.rs
@@ -1,23 +1,34 @@
 use super::movement::{Movement, Location};
+use chrono::Utc;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct EggHopMovement {
     start_location: Location,
     is_grounded: bool,
+    last_update: i64,
 }
 
 impl EggHopMovement {
     pub fn new(start_location: Location) -> Self {
         Self {
             start_location,
-            is_grounded: true 
+            is_grounded: true,
+            last_update: Utc::now().timestamp_millis(),
+        }
+    }
+    
+    fn try_update_state(&mut self) {
+        let now = Utc::now().timestamp_millis();
+        if now - self.last_update > 500 { // half a second
+            self.is_grounded = !self.is_grounded;
+            self.last_update = now;
         }
     }
 }
 
 impl Movement for EggHopMovement {
     fn next_position(&mut self) -> Location {
-        self.is_grounded = !self.is_grounded;
+        self.try_update_state();
         
         if self.is_grounded {
             self.start_location.clone()
