bcdfdc94708b446773efb8319b7cc299ad91bff8
Rik de Goede <rik.dgd@gmail.com>
2024-11-09 01:34:52 +0100

    Create PixelVectorShape

---
+++ src/shapes/pixel_vector.rs
@@ -0,0 +1,27 @@
+use ratatui::widgets::canvas::{Painter, Shape};
+use crate::shapes::PixelImage;
+use crate::utils::Pixel;
+
+pub struct PixelVectorShape(Vec<Pixel>);
+
+impl PixelVectorShape {
+    pub fn new(vector: Vec<Pixel>) -> Self {
+        Self(vector)
+    }
+    
+    pub fn from_pixel_image<T: PixelImage>(pixel_image: &T) -> Self {
+        Self(pixel_image.pixels())
+    }
+}
+
+impl Shape for PixelVectorShape {
+    fn draw(&self, painter: &mut Painter) {
+        for pixel in &self.0 {
+            painter.paint(
+                pixel.x as usize, 
+                pixel.y as usize, 
+                pixel.color,
+            );
+        }
+    }
+}
