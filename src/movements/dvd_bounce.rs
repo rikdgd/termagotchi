16ac2275eb37778594dd35eb3432fcfa8bb3272e
Rik de Goede <rik.dgd@gmail.com>
2024-11-14 00:09:07 +0100

    Make DvdBounceMovement update only once every 1/2 second.

--- src/movements/dvd_bounce.rs
+++ src/movements/dvd_bounce.rs
@@ -6,36 +6,44 @@ pub struct DvdBounceMovement {
     location: Location,
     x_direction_toggle: bool,
     y_direction_toggle: bool,
+    last_update: i64,
 }
 impl DvdBounceMovement {
-    pub fn new(location: Location) -> Self {
+    pub fn new(start_location: Location) -> Self {
         Self {
-            location,
+            location: start_location,
             x_direction_toggle: true,
             y_direction_toggle: true,
+            last_update: Utc::now().timestamp_millis(),
         }
     }
 
-    fn update_state(&mut self) { // TODO: Make based on time.
-        if self.location.x <= 0 || self.location.x >= 40 {
-            self.x_direction_toggle = !self.x_direction_toggle;
-        }
-        if self.location.y <= 0 || self.location.y >= 40 {
-            self.y_direction_toggle = !self.y_direction_toggle;
-        }
+    fn update_state(&mut self) {
+        let now = Utc::now().timestamp_millis();
         
-        let new_x = if self.x_direction_toggle {
-            self.location.x + 1
-        } else {
-            self.location.x - 1 
-        };
-        let new_y = if self.y_direction_toggle {
-            self.location.y + 1
-        } else {
-            self.location.y - 1
-        };
-        
-        self.location = Location::new(new_x, new_y);
+        if now - self.last_update > 500 {
+            self.last_update = now;
+            
+            if self.location.x <= 0 || self.location.x >= 40 {
+                self.x_direction_toggle = !self.x_direction_toggle;
+            }
+            if self.location.y <= 0 || self.location.y >= 40 {
+                self.y_direction_toggle = !self.y_direction_toggle;
+            }
+
+            let new_x = if self.x_direction_toggle {
+                self.location.x + 1
+            } else {
+                self.location.x - 1
+            };
+            let new_y = if self.y_direction_toggle {
+                self.location.y + 1
+            } else {
+                self.location.y - 1
+            };
+
+            self.location = Location::new(new_x, new_y);
+        }
     }
 }
 
