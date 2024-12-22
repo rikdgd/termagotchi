0a71fa926dda96426aa1277477b275839f71f86c
Rik de Goede <rik.dgd@gmail.com>
2024-12-22 01:23:31 +0100

    Add new stat sprites and create method to calculate the amount that should get displayed.

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -2,7 +2,9 @@ pub mod creatures;
 mod pixel_image;
 mod growth_stages;
 mod pixel_vector;
+mod stat_shape;
 
 pub use pixel_vector::PixelVectorShape;
 pub use pixel_image::PixelImage;
 pub use growth_stages::GrowthStageShapes;
+pub use stat_shape::StatShape;
--- src/widgets/canvas_stats.rs
+++ src/widgets/canvas_stats.rs
@@ -2,12 +2,19 @@ use ratatui::{
     style::Color,
     layout::Rect,
     widgets::{
-        canvas::{Canvas, Line, Rectangle},
+        canvas::{Canvas, Line},
         Block,
         Widget
     },
 };
+use ratatui::widgets::canvas::Context;
 use crate::friend::Friend;
+use crate::shapes::{PixelVectorShape, StatShape};
+use crate::utils::Stat;
+
+
+const STAT_SHAPE_WIDTH: u8 = 10;
+
 
 pub struct StatsWidget<'a> {
     friend: &'a Friend,
@@ -34,9 +41,72 @@ impl<'a> StatsWidget<'a> {
                     y2: 10.0,
                     color: Color::White,
                 });
-
+                
+                Self::draw_stats(ctx, self.friend);
             });
 
         canvas
+    }
+    
+    fn draw_stats(ctx: &mut Context, friend: &Friend) {
+        let food_draw = Self::calc_required_symbols(friend.food());
+        let joy_draw = Self::calc_required_symbols(friend.joy());
+        let energy_draw = Self::calc_required_symbols(friend.energy());
+        let waste_draw = Self::calc_required_symbols(friend.waste_level());
+        
+        for i in 0..food_draw {
+            let shape = PixelVectorShape::from_pixel_image(&StatShape::Food)
+                .translate(
+                    (i * STAT_SHAPE_WIDTH) as i32,
+                    0,
+                );
+            
+            ctx.draw(&shape);
+        }
+        
+        todo!()
+    }
+    
+    fn calc_required_symbols(stat: &Stat) -> u8 {
+        match stat.value() {
+            0 => 0,
+            1..=25 => 1,
+            26..=50 => 2,
+            51..=75 => 3,
+            _ => 4,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::utils::Stat;
+    use crate::widgets::StatsWidget;
+
+    #[test]
+    fn calc_required_stat_symbols() {
+        let stat_20 = Stat::new(20).unwrap();
+        let stat_0 = Stat::new(0).unwrap();
+        let stat_100 = Stat::new(100).unwrap();
+        let stat_76 = Stat::new(76).unwrap();
+        let stat_51 = Stat::new(51).unwrap();
+        let stat_26 = Stat::new(26).unwrap();
+        let stat_1 = Stat::new(1).unwrap();
+
+        let result_20 = StatsWidget::calc_required_symbols(&stat_20);
+        let result_0 = StatsWidget::calc_required_symbols(&stat_0);
+        let result_100 = StatsWidget::calc_required_symbols(&stat_100);
+        let result_76 = StatsWidget::calc_required_symbols(&stat_76);
+        let result_51 = StatsWidget::calc_required_symbols(&stat_51);
+        let result_26 = StatsWidget::calc_required_symbols(&stat_26);
+        let result_1 = StatsWidget::calc_required_symbols(&stat_1);
+        
+        assert_eq!(result_20, 1);
+        assert_eq!(result_0, 0);
+        assert_eq!(result_100, 4);
+        assert_eq!(result_76, 4);
+        assert_eq!(result_51, 3);
+        assert_eq!(result_26, 2);
+        assert_eq!(result_1, 1);
     }
 }
