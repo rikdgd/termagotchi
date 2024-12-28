60ed3b139dc531c0ae14e940923fd80ef53d4b74
Rik de Goede <rik.dgd@gmail.com>
2024-12-28 01:09:25 +0100

    Update the hunger and waste stat symbols

--- src/shapes/stat_shape.rs
+++ src/shapes/stat_shape.rs
@@ -13,7 +13,7 @@ impl PixelImage for StatShape {
     fn pixels(&self) -> Vec<Pixel> {
         let color = ColorWrapper::White;
         match self {
-            StatShape::Food => load_embedded_sprite!("../../assets/stat_symbols/burger.png", color),
+            StatShape::Food => load_embedded_sprite!("../../assets/stat_symbols/eat.png", color),
             StatShape::Joy => load_embedded_sprite!("../../assets/stat_symbols/heart.png", color),
             StatShape::Sleep => load_embedded_sprite!("../../assets/stat_symbols/zz.png", color),
             StatShape::Waste => load_embedded_sprite!("../../assets/stat_symbols/toilet_paper.png", color),
