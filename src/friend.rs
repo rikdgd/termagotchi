7167a889ecda22941f825d0a9b628180dd2b2fb8
Rik de Goede <rik.dgd@gmail.com>
2024-12-31 01:27:56 +0100

    Health is now slowly subtracted for a couple of minutes after eating or playing.

--- src/friend.rs
+++ src/friend.rs
@@ -5,6 +5,7 @@ use chrono::Utc;
 use crate::shapes::creatures::CreatureShapes;
 use crate::shapes::{GrowthStageShapes, PixelVectorShape};
 
+const MINUTE_MILLIS: i64 = 1000 * 60;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum GrowthStage {
@@ -38,8 +39,8 @@ pub struct Friend {
     last_time_lower_food: i64,
     last_time_lower_joy: i64,
     last_time_lower_energy: i64,
-    last_time_increase_health: i64,
-    full_health_since: Option<i64>,
+    last_time_lower_health: i64,
+    health_decrease_time_left: i64,
     shape: CreatureShapes,
     growth_stage: GrowthStage,
     asleep: bool,
@@ -59,8 +60,8 @@ impl Friend {
             last_time_lower_food: now,
             last_time_lower_joy: now,
             last_time_lower_energy: now,
-            last_time_increase_health: now,
-            full_health_since: None,
+            last_time_lower_health: now,
+            health_decrease_time_left: 0,
             shape,
             growth_stage: GrowthStage::Egg,
             asleep: false,
@@ -77,72 +78,69 @@ impl Friend {
         
         if self.growth_stage != GrowthStage::Egg {
             self.update_stats(now);
-            self.update_alive_status(now);
+            self.update_alive_status();
         }
     }
     
     fn update_stats(&mut self, now: i64) {
-        let minute_millis = 1000 * 60;
-        
-        let food_offset_minutes = 14;
-        let energy_offset_minutes = 12;
-        let joy_offset_minutes = 16;
+        let food_offset_minutes = 14 * MINUTE_MILLIS;
+        let energy_offset_minutes = 12 * MINUTE_MILLIS;
+        let joy_offset_minutes = 16 * MINUTE_MILLIS;
+        let health_offset_minutes = MINUTE_MILLIS;
 
         // Use while loops instead of if statements to account for loading from file
         // when we might have been away for more than a single minute.
-        while now - self.last_time_lower_food >= food_offset_minutes * minute_millis {
+        while now - self.last_time_lower_food >= food_offset_minutes {
             self.food.subtract(1);
-            self.last_time_lower_food += food_offset_minutes * minute_millis;
+            self.last_time_lower_food += food_offset_minutes;
         }
 
-        while now - self.last_time_lower_energy >= energy_offset_minutes * minute_millis {
+        while now - self.last_time_lower_energy >= energy_offset_minutes {
             match self.asleep {
                 true => self.energy.add(3),
                 false => self.energy.subtract(1),
             }
-            self.last_time_lower_energy += energy_offset_minutes * minute_millis;
+            self.last_time_lower_energy += energy_offset_minutes;
         }
 
-        while now - self.last_time_lower_joy >= joy_offset_minutes * minute_millis {
+        while now - self.last_time_lower_joy >= joy_offset_minutes {
             self.joy.subtract(1);
-            self.last_time_lower_joy += joy_offset_minutes * minute_millis;
+            self.last_time_lower_joy += joy_offset_minutes;
         }
-
-        if self.health.value() >= 100 && self.full_health_since == None {
-            self.full_health_since = Some(now);
-        }
-        if self.health.value() < 100 && self.full_health_since != None {
-            self.full_health_since = None;
+        
+        while now - self.last_time_lower_health >= health_offset_minutes {
+            if self.health_decrease_time_left >= health_offset_minutes {
+                self.health.subtract(1);
+                self.health_decrease_time_left -= health_offset_minutes;
+            }
         }
     }
 
-    fn update_alive_status(&mut self, now: i64) {
-        let stats_sum = self.food.value() + self.joy.value() + self.energy.value();
-        if stats_sum < 15 {
+    fn update_alive_status(&mut self) {
+        let stats_sum = 
+            self.food.value() + 
+            self.joy.value() + 
+            self.energy.value() + 
+            self.health.value();
+        
+        if stats_sum < 20 {
             self.alive = false;
         }
         
         let mut counter: u8 = 0;
-        for stat in [self.food.value(), self.joy.value(), self.energy.value()] {
+        for stat in [self.food.value(), self.joy.value(), self.energy.value(), self.health.value()] {
             if stat == 0 {
                 counter += 1;
             }
         }
-        if counter >= 2 {
+        if counter >= 3 {
             self.alive = false;
-        }
-        
-        if let Some(time) = self.full_health_since {
-            // If the health level has been maxed out for 2 hours.
-            if self.health.value() >= 100 && now - time > 1000 * 60 * 60 * 2 {
-                self.alive = false;
-            }
         }
     }
 
     fn update_growth_stage(&mut self, now: i64) {
         let growth_delay = match self.growth_stage {
-            GrowthStage::Egg => Some(300000),    // 5 minutes
+            GrowthStage::Egg => Some(300000),     // 5 minutes
             GrowthStage::Baby => Some(18000000),  // 5 hours
             GrowthStage::Kid => Some(86400000),   // 24 hours
             GrowthStage::Adult => None,
@@ -165,7 +163,7 @@ impl Friend {
         }
         
         self.food.add(food.points());
-        self.health.add(food.points() / 2);
+        self.health_decrease_time_left += (food.points() / 5) as i64 * MINUTE_MILLIS;
     }
 
     pub fn toggle_sleep(&mut self) {
@@ -177,12 +175,13 @@ impl Friend {
     pub fn play(&mut self) {
         if self.growth_stage != GrowthStage::Egg {
             self.joy.add(30);
+            self.health_decrease_time_left += 6 * MINUTE_MILLIS;
         }
     }
 
     pub fn take_medicine(&mut self) {
         if self.growth_stage != GrowthStage::Egg {
-            self.health.subtract(50);
+            self.health.add(40);
         }
     }
 
