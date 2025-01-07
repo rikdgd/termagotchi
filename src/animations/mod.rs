78752651409237907a57ae7a0bef874a62db7fc2
Rik de Goede <rik.dgd@gmail.com>
2025-01-07 01:20:52 +0100

    Add a sleeping animation when pet is asleep.

--- src/animations/mod.rs
+++ src/animations/mod.rs
@@ -3,9 +3,11 @@ mod animation;
 pub mod food_animation;
 pub mod health_animation;
 pub mod joy_animation;
+mod sleeping_animation;
 
 pub use animation::Animation;
 pub use popup_animation::PopupAnimation;
 
 pub use health_animation::HealthAnimation;
 pub use joy_animation::JoyAnimation;
+pub use sleeping_animation::SleepingAnimation;
