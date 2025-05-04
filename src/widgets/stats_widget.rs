66a8f5f51bb3ced3357d0eff689ca1443ab21e12
Rik de Goede <rik.dgd@gmail.com>
2025-05-05 01:19:56 +0200

    Add and slightly modify example code from ratatui docs for horizontal barchart.

--- src/widgets/stats_widget.rs
+++ src/widgets/stats_widget.rs
@@ -1,38 +1,52 @@
-use ratatui::{prelude::*, widgets::*};
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style, Stylize},
+    text::Line,
+    widgets::{Bar, BarChart, BarGroup, Block},
+    DefaultTerminal, Frame,
+};
 use crate::friend::Friend;
-use crate::utils::Stat;
 
-#[deprecated]
-#[allow(unused)]
-pub fn stats_widget(area: &Rect, friend: &Friend) -> [(Gauge<'static>, Rect); 4] {
-    let layout = layout(area);
+pub struct StatsWidgetGenerator<'a> {
+    friend: &'a Friend,
+}
+impl<'a> StatsWidgetGenerator<'a> {
+    pub fn new(friend: &'a Friend) -> Self {
+        Self {
+            friend
+        }
+    }
     
-    [
-        (stat_gauge(friend.food(), "Food", Color::Red), layout[0]),
-        (stat_gauge(friend.joy(),"Joy", Color::Yellow), layout[1]),
-        (stat_gauge(friend.energy(), "Energy", Color::Blue), layout[2]),
-        (stat_gauge(friend.health(), "Health", Color::Magenta), layout[3]),
-    ]
-}
+    pub fn get_widget(&self, friend: &Friend) -> BarChart {
+        let title = Line::from("Stats").centered();
+        BarChart::default()
+            .block(Block::new().title(title))
+            .data(BarGroup::default().bars(self.get_stat_bars()))
+            .bar_width(1)
+            .bar_gap(0)
+            .direction(Direction::Horizontal)
+    }
+    
+    fn get_stat_bars(&self) -> &[Bar<'a>] {
+        // TODO: Generate the bars based on the creature's stats.
+        todo!()
+    }
 
-fn layout(area: &Rect) -> [Rect; 4] {
-    Layout::vertical([
-        Constraint::Percentage(25),
-        Constraint::Percentage(25),
-        Constraint::Percentage(25),
-        Constraint::Percentage(25),
-    ])
-        .areas(*area)
-}
+    fn horizontal_bar(stat_name: &str) -> Bar {
+        let style = Self::temperature_style(50);
+        Bar::default()
+            .value(u64::from(50u64))
+            .label(Line::from("bar_label"))
+            .text_value("text_value".to_string())
+            .style(style)
+            .value_style(style.reversed())
+    }
 
-fn stat_gauge(stat: &Stat, title: &'static str, color: Color) -> Gauge<'static> {
-    Gauge::default()
-        .block(Block::new().title(title))
-        .gauge_style(
-            Style::default()
-                .fg(color)
-                // .bg(Color::Black)
-                .add_modifier(Modifier::ITALIC),
-        )
-        .percent(stat.value() as u16)
+    /// create a yellow to red value based on the value (50-90)
+    fn temperature_style(value: u8) -> Style {
+        let green = (255.0 * (1.0 - f64::from(value - 50) / 40.0)) as u8;
+        let color = Color::Rgb(255, green, 0);
+        Style::new().fg(color)
+    }
 }
