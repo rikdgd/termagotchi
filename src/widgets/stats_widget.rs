8a90bd59eceb6aae38649613b51eae347b77855e
Rik de Goede <rik.dgd@gmail.com>
2025-05-06 00:12:14 +0200

    The "Stats widget" now uses the values from the current friend instead of fixed values. Styling is also adjusted to use the stat's value.

--- src/widgets/stats_widget.rs
+++ src/widgets/stats_widget.rs
@@ -7,45 +7,60 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 use crate::friend::Friend;
+use crate::utils::Stat;
 
-pub struct StatsWidgetGenerator<'a> {
+/// Manages the state of the *"Stats widget"* and allows for generating the widget so it can be 
+/// rendered in the TUI.
+/// ## Fields:
+/// * `friend` - A reference to the current Termagotchi pet, used to get the stats of the pet.
+/// * `bars` - The `ratatui::widgets::Bar` items that should be displayed in the widget, each corresponding
+/// a specific creature stat.
+pub struct StatsWidgetManager<'a> {
     friend: &'a Friend,
+    bars: [Bar<'a>; 4],
 }
-impl<'a> StatsWidgetGenerator<'a> {
+impl<'a> StatsWidgetManager<'a> {
     pub fn new(friend: &'a Friend) -> Self {
         Self {
-            friend
+            friend,
+            bars: Self::generate_stat_bars(friend),
         }
     }
+
+    fn generate_stat_bars(friend: &Friend) -> [Bar<'a>; 4] {
+        [
+            Self::generate_stat_bar("Food", *friend.food()),
+            Self::generate_stat_bar("Joy", *friend.joy()),
+            Self::generate_stat_bar("Energy", *friend.energy()),
+            Self::generate_stat_bar("Health", *friend.health()),
+        ]
+    }
     
-    pub fn get_widget(&self, friend: &Friend) -> BarChart {
+    /// Returns the widget that can be rendered in the TUI with all the stats of the creature displayed.
+    pub fn get_widget(&self) -> BarChart {
         let title = Line::from("Stats").centered();
         BarChart::default()
             .block(Block::new().title(title))
-            .data(BarGroup::default().bars(self.get_stat_bars()))
+            .data(BarGroup::default().bars(&self.bars))
             .bar_width(1)
             .bar_gap(0)
             .direction(Direction::Horizontal)
     }
-    
-    fn get_stat_bars(&self) -> &[Bar<'a>] {
-        // TODO: Generate the bars based on the creature's stats.
-        todo!()
-    }
 
-    fn horizontal_bar(stat_name: &str) -> Bar {
-        let style = Self::temperature_style(50);
+    fn generate_stat_bar(stat_name: &str, stat: Stat) -> Bar {
+        // Using the stat.value() as a u8 here is safe, since the stats value can at max be 100.
+        let style = Self::stat_style(stat);
         Bar::default()
-            .value(u64::from(50u64))
+            .value(u64::from(stat.value()))
             .label(Line::from("bar_label"))
-            .text_value("text_value".to_string())
+            .text_value(stat_name.to_string())
             .style(style)
             .value_style(style.reversed())
     }
 
     /// create a yellow to red value based on the value (50-90)
-    fn temperature_style(value: u8) -> Style {
-        let green = (255.0 * (1.0 - f64::from(value - 50) / 40.0)) as u8;
+    fn stat_style(stat: Stat) -> Style {
+        let green = stat.value() as u8;
         let color = Color::Rgb(255, green, 0);
         Style::new().fg(color)
     }
